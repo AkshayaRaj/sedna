4c4deb76f51493810f46686bfffc9f946d0c4623
Akshaya Raj <ard.astro@gmail.com>
2015-05-06 16:46:33 +0530

    fix sim bug

--- eclipsify.sh
+++ eclipsify.sh
@@ -1,3 +1,3 @@
 catkin_make --force-cmake -G"Eclipse CDT4 - Unix Makefiles"
 
-awk -f $(rospack find mk)/eclipse.awk build/.project > build/.project_with_env && mv build/.project_with_env build/.project
+sudo awk -f $(rospack find mk)/eclipse.awk build/.project > build/.project_with_env && mv build/.project_with_env build/.project
